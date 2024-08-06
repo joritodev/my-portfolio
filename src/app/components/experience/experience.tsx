6158fc8ce2e616822bfd864a458ff44525b20952
Joritodev <joritodev@gmail.com>
2024-08-06 13:49:26 -0300

    feat: finish project

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,11 +1,50 @@
+import Image from "next/image"
 import { SectionTitle } from "../sectionTitle/section-title"
 import "./experience.scss"
+import { TSIcon } from "../icons/typescript-icon"
+import { ReactIcon } from "../icons/react-icon"
+import { JSIcon } from "../icons/javascript-icon"
+import { SassIcon } from "../icons/sass-icon"
+
 export function Experience() {
     return (
-            <div className="experience">
-                <SectionTitle text="Experience"/>
-                <p>Faço projetos front-end através de desafios do site Frontend Mentor</p>
-                <div className="experience-time"></div>
+        <div className="experience">
+            <SectionTitle text="Experiência" />
+            <p>Faço projetos front-end através de desafios do site Frontend Mentor</p>
+            <div className="experience-time">
+                <div className="experience-language">
+                    <a>
+                        <ReactIcon />
+                    </a>
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1"><span>3 meses</span></div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <a>
+                        <JSIcon />
+                    </a>
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-3"><span>9 meses</span></div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <a>
+                        <TSIcon />
+                    </a>
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1"><span>3 meses</span></div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <a>
+                        <SassIcon />
+                    </a>
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-3"><span>9 meses</span></div>
+                    </div>
+                </div>
             </div>
+        </div>
     )
 }
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import "./header.scss"
 export function Header() {
     return (
+
             <div className="header">
                 <div>
                     <h1>Oii, eu sou João Victor 👋</h1>
@@ -10,8 +11,8 @@ export function Header() {
                 <Image
                     src="/Jorito.png"
                     alt="Jorito Foto"
-                    width={325}
-                    height={277}
+                    width={220}
+                    height={220}
                     priority
                 />
             </div>
